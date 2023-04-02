d9180cc0fb53eeba6779fff397ee971e9381b851
Daris Cappelletti <daris.cappelletti@regione.marche.it>
2023-04-02 11:01:40 +0200

    Update

---
+++ GestioneComande/ComandeForm.cs
@@ -0,0 +1,26 @@
+﻿using MaterialSkin.Controls;
+using System.Windows.Forms;
+
+namespace GestioneComande
+{
+    public partial class ComandeForm : MaterialForm
+    {
+        readonly MaterialSkin.MaterialSkinManager materialSkinManager;
+        public ComandeForm()
+        {
+            InitializeComponent();
+            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.EnforceBackcolorOnAllComponents = true;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme =
+                new MaterialSkin.ColorScheme(
+                    MaterialSkin.Primary.Indigo500,
+                    MaterialSkin.Primary.Indigo700,
+                    MaterialSkin.Primary.Indigo100,
+                    MaterialSkin.Accent.Pink200,
+                    MaterialSkin.TextShade.WHITE
+                    );
+        }
+    }
+}
